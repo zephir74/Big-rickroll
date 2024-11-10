e8f8090acd5d9d16d6cb933d922d28d2e37aaf7d
zephir <zephir.hgfl@proton.me>
2024-11-10 23:16:13 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,8 @@
-at 11:00 04/01/2025 smb.bat
+@echo off
+cls
+set "date=04/01"
+set "hour=11:00"
+
+at %hour% %date%/2025 smb.bat
+echo The rickroll will start the %date% 2025 at %hour%
+echo Thanks for launching this program
