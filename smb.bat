32e0332c55d9dfcb09ea9ef6704088030ac90de5
zephir <zephir.hgfl@proton.me>
2024-11-10 15:46:38 +0100

    Add files via upload

---
+++ smb.bat
@@ -0,0 +1,21 @@
+@echo off
+cls
+
+setlocal enabledelayedexpansion
+
+set "subnet=" :: 192.168.1 for example
+
+set "targets="
+
+for /L %%i in (1,1,254) do (
+    set "ip=%subnet%.%%i"
+
+    ping -n 1 -w 1000 %ip% >nul
+    if !errorlevel! == 0 (
+        set "targets=!targets! %ip%"
+    )
+)
+
+for /f "tokens=*" %%A in (%targets%) do (
+    net use \\%%A\path\to\rickroll.bat /user:username password
+)
